4dea7d44421707b17e8f7ecac22c625ad23f2705
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-06-21 17:25:04 +0500

    Refactor Quiz component to improve answer state management and code readability

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,43 +5,64 @@ import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+  const [answerState, setAnswerState] = useState("");
+  const activeQuestionIndex =
+   answerState === "" ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswers) {
+  const handleSelectAnswer = useCallback(
+   function handleSelectAnswer(selectedAnswers) {
+    setAnswerState("answered");
     setUserAnswers((prev) => {
-        return[
-          ...prev,
-          selectedAnswers
-        ]
-    })
-  },[]);
-  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null),[handleSelectAnswer]);
-  if(quizIsComplete) {
-    return (<div id="summary">
-      <img src={quizCompleteImage}/>
-      <h2>Quiz Summary</h2>
-    </div>)
+     return [...prev, selectedAnswers];
+    });
+    setTimeout(() => {
+     if (selectedAnswers === QUESTIONS[activeQuestionIndex].answers[0]) {
+      setAnswerState("correct");
+     } else {
+      setAnswerState("wrong");
+     }
+     setTimeout(() => {
+      setAnswerState("");
+     }, 2000);
+    }, 1000);
+   },
+   [activeQuestionIndex],
+  );
+  const handleSkipAnswer = useCallback(
+   () => handleSelectAnswer(null),
+   [handleSelectAnswer],
+  );
+  if (quizIsComplete) {
+   return (
+    <div id="summary">
+     <img src={quizCompleteImage} />
+     <h2>Quiz Summary</h2>
+    </div>
+   );
   }
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() =>Math.random()-0.5);
-  return(
-    <>
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+  return (
+   <>
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer time={10000} onTimeout={() => handleSelectAnswer(null)} />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers
-          .map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+     <div id="question">
+      <QuestionTimer
+       key={activeQuestionIndex}
+       time={10000}
+       onTimeout={() => handleSelectAnswer(null)}
+      />
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+       {shuffledAnswers.map((answer) => (
+        <li
+         key={answer}
+         className="answer">
+         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+        </li>
+       ))}
+      </ul>
+     </div>
     </div>
-    </>
-  )
+   </>
+  );
 }
